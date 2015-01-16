899bf1e2a3bad23f5cea1d883b2caa92cecbfbd4
Makoa Jacobsen <makoa@makoajacobsen.com>
2015-01-16 16:55:25 +0000

    Navigation Loading Changes

--- src/Core/Component/Navigation/Navigation.php
+++ src/Core/Component/Navigation/Navigation.php
@@ -5,6 +5,7 @@ namespace Kula\Core\Component\Navigation;
 class Navigation {
   
   private $navigation = array();
+  private $navigationArranged = array();
   
   private $db;
   
@@ -13,7 +14,31 @@ class Navigation {
   }
   
   public function loadNavigation() {
-    return array();
+    
+    $navResults = $this->db->db_select('CORE_NAVIGATION', 'navigation')
+      ->fields('navigation')
+      ->leftJoin('CORE_NAVIGATION', 'parent_nav', 'parent_nav.NAVIGATION_ID = navigation.PARENT_NAVIGATION_ID')
+      ->fields('parent_nav', array('NAVIGATION_NAME' => 'parent_NAVIGATION_NAME'))
+      ->orderBy('navigation.PORTAL')
+      ->orderBy('navigation.NAVIGATION_TYPE')
+      ->orderBy('navigation.SORT')
+      ->execute();
+    while ($navRow = $navResults->fetch()) {
+      $navigationItem = new NavigationItem($navRow['NAVIGATION_NAME'], $navRow['parent_NAVIGATION_NAME'], $navRow['NAVIGATION_ID'], $navRow['NAVIGATION_TYPE'], $navRow['PORTAL'], $navRow['SORT'], $navRow['DISPLAY_NAME'], $navRow['DIVIDER_BEFORE'], $navRow['RECORD_LOADED'], $navRow['ROUTE'], $navRow['CONFIRMATION_MESSAGE']);
+      
+      $this->navigation[$navRow['NAVIGATION_NAME']] = $navigationItem;
+      
+      $this->navigationArranged[$navRow['PORTAL']][$navRow['NAVIGATION_TYPE']][$navRow['NAVIGATION_NAME']] = $navigationItem;
+      
+      unset($navigationItem);
+      
+    }
+  }
+  
+  public function getNavigationForPortal($portal, $type) {
+    
+    return $this->navigationArranged[$portal][$type];
+    
   }
   
   public function __sleep() {
