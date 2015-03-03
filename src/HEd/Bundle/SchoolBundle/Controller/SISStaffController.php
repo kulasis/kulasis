8b1ec7dda1daa3c092d0fd6ec9d6213b5f8f3d2b
Makoa Jacobsen <makoa@makoajacobsen.com>
2015-03-03 15:34:12 -0800

    Additional staff fixes.

--- src/HEd/Bundle/SchoolBundle/Controller/SISStaffController.php
+++ src/HEd/Bundle/SchoolBundle/Controller/SISStaffController.php
@@ -39,42 +39,6 @@ class SISStaffController extends Controller {
     return $this->render('KulaHEdSchoolBundle:SISStaff:orgterms.html.twig', array('stafforgterms' => $stafforgterms));  
   }
   
-  public function staff_scheduleAction() {
-    $this->authorize();
-    $this->processForm();
-    $this->setRecordType('SIS.HEd.Staff.SchoolTerm');
-    
-    $classes = array();
-    
-    if ($this->record->getSelectedRecordID()) {
-    
-    $classes = $this->db()->db_select('STUD_SECTION', 'section')
-      ->fields('section', array('SECTION_ID', 'SECTION_NUMBER'))
-      ->join('STUD_COURSE', 'course', array('COURSE_NUMBER', 'COURSE_TITLE'), 'course.COURSE_ID = section.COURSE_ID')
-      ->leftJoin('STUD_STAFF_ORGANIZATION_TERMS', 'stafforgtrm', null, 'section.STAFF_ORGANIZATION_TERM_ID = stafforgtrm.STAFF_ORGANIZATION_TERM_ID')
-      ->leftJoin('STUD_SECTION_MEETINGS', 'meetings', array('MON','TUE','WED','THU','FRI','SAT','SUN', 'START_TIME', 'END_TIME'), 'meetings.SECTION_ID = section.SECTION_ID')
-      ->leftJoin('STUD_ROOM', 'rooms', array('ROOM_NUMBER'), 'rooms.ROOM_ID = meetings.ROOM_ID')
-      ->leftJoin('STUD_STAFF', 'staff', array('ABBREVIATED_NAME'), 'staff.STAFF_ID = stafforgtrm.STAFF_ID')
-      ->condition('section.STAFF_ORGANIZATION_TERM_ID', $this->record->getSelectedRecordID())
-      ->orderBy('SECTION_NUMBER', 'ASC')
-      ->execute()->fetchAll();
-    
-      foreach($classes as $key => $class) {
-        $classes[$key]['meets'] = '';
-        if ($class['MON'] == 'Y') $classes[$key]['meets'] .= 'M';
-        if ($class['TUE'] == 'Y') $classes[$key]['meets'] .= 'T';
-        if ($class['WED'] == 'Y') $classes[$key]['meets'] .= 'W';
-        if ($class['THU'] == 'Y') $classes[$key]['meets'] .= 'R';
-        if ($class['FRI'] == 'Y') $classes[$key]['meets'] .= 'F';
-        if ($class['SAT'] == 'Y') $classes[$key]['meets'] .= 'S';
-        if ($class['SUN'] == 'Y') $classes[$key]['meets'] .= 'U';
-      }
-    
-    }
-    
-    return $this->render('schedule.html.twig', array('classes' => $classes));  
-  }
-  
   public function staff_chooserAction() {
     $this->authorize();
     $data = $this->chooser('HEd.Staff')->createChooserMenu($this->request->query->get('q'));
