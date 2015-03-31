c0089896db063cf1ce0e34111c1442991ed06175
Makoa Jacobsen <makoa@makoajacobsen.com>
2015-03-31 14:57:47 -0700

    Fix home holds controller to new standard.

--- src/HEd/Bundle/StudentBundle/Controller/SISHomeHoldsController.php
+++ src/HEd/Bundle/StudentBundle/Controller/SISHomeHoldsController.php
@@ -15,30 +15,34 @@ class SISHomeHoldsController extends Controller {
       
       $items_to_void = $this->request->request->get('post');
       
-      $this->db()->beginTransaction();
+      $transaction = $this->db()->db_transaction();
       
       foreach($items_to_void as $table => $table_row) {
         foreach($table_row as $row_id => $row) {
-          $void_poster = new \Kula\Component\Database\Poster(null, array('STUD_STUDENT_HOLDS' => array($row_id => array('VOIDED' => array('checkbox' => 'Y', 'checkbox_hidden' => ''), 'VOIDED_TIMESTAMP' => date('Y-m-d H:i:s'), 'VOIDED_USERSTAMP' => $this->session->get('user_id')))));
-          unset($data);
+          $void_poster = $this->newPoster()->edit('HEd.Student.Hold', $row_id, array(
+            'HEd.Student.Hold.Voided' => 1,
+            'HEd.Student.Hold.VoidedTimestamp' => date('Y-m-d H:i:s'),
+            'HEd.Student.Hold.VoidedUserstamp' => $this->session->get('user_id')
+          ))->process();
         }
       }
       
-      $this->db()->commit();
+      $transaction->commit();
     } 
     
     $holds = array();
     
     if ($this->record->getSelectedRecordID()) {
     
-    $holds = $this->db()->select('STUD_STUDENT_HOLDS', 'stuholds')
+    $holds = $this->db()->db_select('STUD_STUDENT_HOLDS', 'stuholds')
       ->fields('stuholds', array('STUDENT_HOLD_ID', 'HOLD_ID', 'HOLD_DATE', 'COMMENTS', 'VOIDED_REASON'))
-      ->join('CONS_CONSTITUENT', 'constituent', array('LAST_NAME', 'FIRST_NAME', 'PERMANENT_NUMBER', 'GENDER'), 'constituent.CONSTITUENT_ID = stuholds.STUDENT_ID')
-      ->predicate('stuholds.VOIDED', 'N')
-      ->predicate('stuholds.HOLD_ID', $this->record->getSelectedRecordID())
-      ->order_by('LAST_NAME', 'ASC', 'constituent')
-      ->order_by('FIRST_NAME', 'ASC', 'constituent')
-      ->order_by('PERMANENT_NUMBER', 'ASC', 'constituent')
+      ->join('CONS_CONSTITUENT', 'constituent', 'constituent.CONSTITUENT_ID = stuholds.STUDENT_ID')
+      ->fields('constituent', array('LAST_NAME', 'FIRST_NAME', 'PERMANENT_NUMBER', 'GENDER'))
+      ->condition('stuholds.VOIDED', 0)
+      ->condition('stuholds.HOLD_ID', $this->record->getSelectedRecordID())
+      ->orderBy('constituent.LAST_NAME', 'ASC')
+      ->orderBy('constituent.FIRST_NAME', 'ASC')
+      ->orderBy('constituent.PERMANENT_NUMBER', 'ASC')
       ->execute()->fetchAll();
     }
     
