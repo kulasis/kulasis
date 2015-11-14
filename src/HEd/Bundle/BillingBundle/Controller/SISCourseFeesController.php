adbbe32f94b8f388545dac102b5be8c2c305651c
Makoa Jacobsen <makoa@makoajacobsen.com>
2015-11-14 15:51:12 -0800

    Add CP fee refund schema changes and screens to courses and sections.

--- src/HEd/Bundle/BillingBundle/Controller/SISCourseFeesController.php
+++ src/HEd/Bundle/BillingBundle/Controller/SISCourseFeesController.php
@@ -12,14 +12,30 @@ class SISCourseFeesController extends Controller {
     $this->setRecordType('SIS.HEd.Course');
     
     $fees = array();
+    $refund_fees = array();
     
     if ($this->record->getSelectedRecordID()) {
       
-      // Get Rooms
+      // Get Course Fees
       $fees = $this->db()->db_select('BILL_COURSE_FEE', 'BILL_COURSE_FEE')
         ->fields('BILL_COURSE_FEE', array('AMOUNT', 'CODE_ID', 'COURSE_FEE_ID'))
         ->join('CORE_ORGANIZATION_TERMS', 'orgterms', 'orgterms.ORGANIZATION_TERM_ID = BILL_COURSE_FEE.ORGANIZATION_TERM_ID')
         ->join('BILL_CODE', 'code', 'code.CODE_ID = BILL_COURSE_FEE.CODE_ID')
+        ->fields('code', array('CODE_DESCRIPTION'))
+        ->join('CORE_TERM', 'term', 'term.TERM_ID = orgterms.TERM_ID')
+        ->fields('term', array('TERM_ABBREVIATION'))
+        ->join('CORE_ORGANIZATION', 'org', 'org.ORGANIZATION_ID = orgterms.ORGANIZATION_ID')
+        ->fields('org', array('ORGANIZATION_NAME'))
+        ->condition('COURSE_ID', $this->record->getSelectedRecordID())
+        ->condition('orgterms.ORGANIZATION_TERM_ID', $this->focus->getOrganizationTermID())
+        ->orderBy('term.START_DATE', 'DESC')
+        ->orderBy('CODE_DESCRIPTION', 'ASC')
+      ->execute()->fetchAll();
+    
+      $refund_fees = $this->db()->db_select('BILL_COURSE_FEE_REFUND', 'BILL_COURSE_FEE_REFUND')
+        ->fields('BILL_COURSE_FEE_REFUND', array('AMOUNT', 'CODE_ID', 'END_DATE', 'COURSE_FEE_REFUND_ID'))
+        ->join('CORE_ORGANIZATION_TERMS', 'orgterms', 'orgterms.ORGANIZATION_TERM_ID = BILL_COURSE_FEE_REFUND.ORGANIZATION_TERM_ID')
+        ->join('BILL_CODE', 'code', 'code.CODE_ID = BILL_COURSE_FEE_REFUND.CODE_ID')
         ->fields('code', array('CODE_DESCRIPTION'))
         ->join('CORE_TERM', 'term', 'term.TERM_ID = orgterms.TERM_ID')
         ->fields('term', array('TERM_ABBREVIATION'))
@@ -41,7 +57,7 @@ class SISCourseFeesController extends Controller {
       ->condition('ORGANIZATION_TERM_ID', $this->focus->getOrganizationTermID())
       ->execute()->fetch();
     
-    return $this->render('KulaHEdBillingBundle:SISCourseFees:fees_index.html.twig', array('fees' => $fees, 'organization_term_id' => $this->focus->getOrganizationTermID(), 'organization_term_id_display' => $org_term['TERM_ABBREVIATION'] . ' / ' . $org_term['ORGANIZATION_NAME']));
+    return $this->render('KulaHEdBillingBundle:SISCourseFees:fees_index.html.twig', array('fees' => $fees, 'refund_fees' => $refund_fees, 'organization_term_id' => $this->focus->getOrganizationTermID(), 'organization_term_id_display' => $org_term['TERM_ABBREVIATION'] . ' / ' . $org_term['ORGANIZATION_NAME']));
     
   }
   
@@ -51,6 +67,7 @@ class SISCourseFeesController extends Controller {
     $this->setRecordType('SIS.HEd.Section');
     
     $fees = array();
+    $refund_fees = array();
     
     if ($this->record->getSelectedRecordID()) {
       
@@ -62,9 +79,17 @@ class SISCourseFeesController extends Controller {
         ->orderBy('CODE_DESCRIPTION', 'ASC')
       ->execute()->fetchAll();
       
+      $refund_fees = $this->db()->db_select('BILL_SECTION_FEE_REFUND', 'BILL_SECTION_FEE')
+        ->fields('BILL_SECTION_FEE', array('END_DATE', 'AMOUNT', 'CODE_ID', 'SECTION_FEE_REFUND_ID'))
+        ->join('BILL_CODE', 'code', 'code.CODE_ID = BILL_SECTION_FEE.CODE_ID')
+        ->fields('code', array('CODE_DESCRIPTION'))
+        ->condition('SECTION_ID', $this->record->getSelectedRecordID())
+        ->orderBy('CODE_DESCRIPTION', 'ASC')
+      ->execute()->fetchAll();
+      
     }
     
-    return $this->render('KulaHEdBillingBundle:SISCourseFees:section.html.twig', array('fees' => $fees));
+    return $this->render('KulaHEdBillingBundle:SISCourseFees:section.html.twig', array('fees' => $fees, 'refund_fees' => $refund_fees));
     
   }
 
