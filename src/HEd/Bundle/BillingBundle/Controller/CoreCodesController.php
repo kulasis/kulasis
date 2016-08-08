1d2da0136a4fd9b402138a74d3f1180b30a90705
Makoa Jacobsen <makoa@makoajacobsen.com>
2016-08-08 08:23:02 -0700

    Fix ability to add/edit billing codes.

--- src/HEd/Bundle/BillingBundle/Controller/CoreCodesController.php
+++ src/HEd/Bundle/BillingBundle/Controller/CoreCodesController.php
@@ -8,14 +8,55 @@ class CoreCodesController extends Controller {
   
   public function codesAction() {
     $this->authorize();
+    $this->setRecordType('Core.HEd.BillingCode');
     $this->processForm();
     
-    $codes = $this->db()->db_select('BILL_CODE')
-      ->fields('BILL_CODE')
-      ->orderBy('CODE')
-      ->execute()->fetchAll();
+    $code = array();
+    if ($this->record->getSelectedRecordID()) {
+
+	    $code = $this->db()->db_select('BILL_CODE')
+	      ->fields('BILL_CODE')
+	      ->condition('CODE_ID', $this->record->getSelectedRecordID())
+	      ->execute()->fetchAll();
+    }
     
-    return $this->render('KulaHEdBillingBundle:CoreCodes:codes.html.twig', array('codes' => $codes));
+    return $this->render('KulaHEdBillingBundle:CoreCodes:codes.html.twig', array('code' => $code));
+  }
+
+  public function addAction() {
+    $this->authorize();
+    $this->setRecordType('Core.HEd.BillingCode', 'Y');
+    $this->formAction('Core_HEd_Billing_BillingSetup_Codes_Create');
+    return $this->render('KulaHEdBillingBundle:CoreCodes:add.html.twig');
+  }
+  
+  public function createAction() {
+    $this->authorize();
+    $this->processForm();
+    $id = $this->poster()->getResult();
+    
+    if ($id) {
+      $this->addFlash('success', 'Created section.');
+      return $this->forward('Core_HEd_Billing_BillingSetup_Codes', array('record_type' => 'Core.HEd.BillingCode', 'record_id' => $id), array('record_type' => 'Core.HEd.BillingCode', 'record_id' => $id));
+    } else {
+    	$this->formAction('Core_HEd_Billing_BillingSetup_Codes_Create');
+    	return $this->render('KulaHEdBillingBundle:CoreCodes:add.html.twig');
+    }
+
+  }
+  
+  public function deleteAction() {
+    $this->authorize();
+    $this->setRecordType('Core.HEd.BillingCode');
+    
+    $rows_affected = $this->db()->db_delete('BILL_CODE')
+        ->condition('CODE_ID', $this->record->getSelectedRecordID())->execute();
+    
+    if ($rows_affected == 1) {
+      $this->addFlash('success', 'Deleted billing code.');
+    }
+    
+    return $this->forward('Core_HEd_Billing_BillingSetup_Codes');
   }
   
 }
---
+++ src/HEd/Bundle/BillingBundle/Record/CoreBillingCodeRecord.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Kula\HEd\Bundle\BillingBundle\Record;
+
+use Kula\Core\Component\Record\Record;
+
+class CoreBillingCodeRecord extends Record {
+  
+  public function getSelectedRecordBarTemplate() {
+    
+  }
+  
+  public function getRecordBarTemplate() {
+    return 'KulaHEdBillingBundle::CoreRecord/record_bill_code.html.twig';
+  }
+  
+  public function getRecordIDStack() {
+    
+    $result = $this->db()->db_select('BILL_CODE', 'billcode')
+    ->fields('billcode', array('CODE_ID' => 'ID'))
+    ->orderBy('CODE', 'ASC');
+    $result = $result->execute()->fetchAll();
+    
+    return $result;    
+  }
+  
+  public function get($record_id) {
+    
+    $result = $this->db()->db_select('BILL_CODE', 'billcode')
+    ->fields('billcode', array('CODE_ID', 'CODE_TYPE', 'CODE', 'CODE_DESCRIPTION'))
+    ->condition('billcode.CODE_ID', $record_id)
+    ->execute()->fetch();
+    return $result;
+    
+  }
+  
+  public function getBaseTable() {
+    return 'HEd.Billing.Code';
+  }
+  
+  public function getBaseKeyFieldName() {
+    return 'HEd.Billing.Code.ID';
+  }
+  
+  public function modifySearchDBOBject($db_obj) {
+    return $db_obj;
+  }
+
+}
