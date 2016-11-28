6aa5d924c578ceffcf3a325dee57067a8d47e534
Makoa Jacobsen <makoa@makoajacobsen.com>
2016-11-27 20:21:04 -0800

    Add billing statement.

--- src/Core/Bundle/BillingBundle/Controller/APIv1PaymentController.php
+++ src/Core/Bundle/BillingBundle/Controller/APIv1PaymentController.php
@@ -13,6 +13,46 @@ use GuzzleHttp\Exception\ClientException;
 
 class APIv1PaymentController extends APIController {
 
+  public function getStatementAction($org) {
+
+    // get logged in user
+    $currentUser = $this->authorizeUser();
+
+    $data = array();
+
+    $data['total'] = 0;
+    $i = 0;
+    $transactions_result = $this->db()->db_select('BILL_CONSTITUENT_TRANSACTIONS', 'transactions')
+      ->fields('transactions', array('CONSTITUENT_TRANSACTION_ID', 'TRANSACTION_DATE', 'TRANSACTION_DESCRIPTION', 'AMOUNT', 'POSTED', 'VOIDED', 'APPLIED_BALANCE', ))
+      ->join('BILL_CODE', 'code', 'code.CODE_ID = transactions.CODE_ID')
+      ->fields('code', array('CODE_TYPE', 'CODE'))
+      ->leftJoin('CORE_ORGANIZATION_TERMS', 'orgterms', 'orgterms.ORGANIZATION_TERM_ID = transactions.ORGANIZATION_TERM_ID')
+      ->leftJoin('CORE_ORGANIZATION', 'org', 'org.ORGANIZATION_ID = orgterms.ORGANIZATION_ID')
+      ->fields('org', array('ORGANIZATION_ABBREVIATION'))
+      ->leftJoin('CORE_TERM', 'term', 'term.TERM_ID = orgterms.TERM_ID')
+      ->fields('term', array('TERM_ABBREVIATION'))
+      ->leftJoin('STUD_STUDENT_CLASSES', 'stuclass', 'stuclass.STUDENT_CLASS_ID = transactions.STUDENT_CLASS_ID')
+      ->leftJoin('STUD_SECTION', 'sec', 'sec.SECTION_ID = stuclass.SECTION_ID')
+      ->fields('sec', array('SECTION_NUMBER', 'SECTION_ID'))
+      ->leftJoin('STUD_COURSE', 'crs', 'crs.COURSE_ID = sec.COURSE_ID')
+      ->fields('crs', array('COURSE_TITLE'))
+      ->leftJoin('BILL_CONSTITUENT_PAYMENTS', 'payments', 'payments.CONSTITUENT_PAYMENT_ID = transactions.PAYMENT_ID')
+      ->fields('payments', array('PAYMENT_TYPE', 'PAYMENT_DATE', 'PAYMENT_NUMBER'))
+      ->condition('transactions.CONSTITUENT_ID', $currentUser)
+      ->condition('transactions.POSTED', 1)
+      ->condition('transactions.SHOW_ON_STATEMENT', 1)
+      ->condition('org.ORGANIZATION_ABBREVIATION', $org)
+      ->orderBy('TRANSACTION_DATE', 'DESC', 'transactions')
+      ->execute();
+    while ($transaction_row = $transactions_result->fetch()) {
+      $data['transactions'][$i] = $transaction_row;
+      $data['total'] += $transaction_row['AMOUNT'];
+    $i++;
+    }
+
+    return $this->jsonResponse($transactions);
+  }
+
   public function processPaymentAction() {
 
     // get logged in user
