86298f6ce33f0bf7aaa961f52077c57d580b9572
Makoa Jacobsen <makoa@makoajacobsen.com>
2017-05-21 18:35:13 -0700

    Add failed login attempts to login.

--- src/Core/Bundle/LoginBundle/Service/LoginService.php
+++ src/Core/Bundle/LoginBundle/Service/LoginService.php
@@ -36,18 +36,19 @@ class LoginService {
     if (($username && $password) || $this->auth_google->getEmailAddress()) {
       // get password for username
       $result = $this->db->db_select('CORE_USER', 'user')
-        ->fields('user', array('USER_ID', 'USERNAME', 'PASSWORD', 'ALLOW_AUTH_LOCAL', 'ALLOW_AUTH_LDAP', 'ALLOW_AUTH_GOOGLE'))
+        ->fields('user', array('USER_ID', 'USERNAME', 'PASSWORD', 'ALLOW_AUTH_LOCAL', 'ALLOW_AUTH_LDAP', 'ALLOW_AUTH_GOOGLE', 'FAILED_ATTEMPTS'))
         ->leftJoin('CORE_SYSTEM_LDAP', 'ldap', 'ldap.LDAP_ID = user.LDAP_ID')
         ->fields('ldap', array('SERVER_NAME', 'SERVER_ADDRESS', 'DOMAIN_APPEND'))
         ->condition($or_predicate)
         ->execute()->fetch();
       
       // Make sure username exists
-      if ($result['USERNAME']) {
+      if ($result['USERNAME'] AND $result['FAILED_ATTEMPTS'] <= 3) {
         // Authenticate using authentication method; Priority: Local, LDAP, Google API
         if ($result['ALLOW_AUTH_LOCAL'] == 1) {
           if ($this->auth_local->verifyPassword($password, $result['PASSWORD'])) {
             $this->establishSession($result['USER_ID']);
+            $this->resetFailedLoginAttempt($result['USER_ID']);
             return true;
           }
         }
@@ -57,6 +58,7 @@ class LoginService {
             if ($this->auth_ldap->authenticate($result['USERNAME'], $password, $result['SERVER_ADDRESS'], $result['SERVER_NAME'], $result['DOMAIN_APPEND'])) {
 
               $this->establishSession($result['USER_ID']);
+              $this->resetFailedLoginAttempt($result['USER_ID']);
               return true;
             }
 
@@ -68,13 +70,24 @@ class LoginService {
         if ($result['ALLOW_AUTH_GOOGLE'] == 1) {
           if ($email = $this->auth_google->getEmailAddress()) {
             $this->establishSession($result['USER_ID']);
+            $this->resetFailedLoginAttempt($result['USER_ID']);
             return true;
           }
         }
+        // Didn't authenticate; log failed attempt
+        $this->addFailedLoginAttempt($result['USER_ID'], $result['FAILED_ATTEMPTS']);
           
       }
     }
     return false;
+  }
+
+  public function addFailedLoginAttempt($user_id, $failed_attempts) {
+    $this->db->db_update('CORE_USER')->fields(array('FAILED_ATTEMPTS' => $failed_attempts + 1))->condition('USER_ID', $user_id)->execute();
+  }
+
+  public function resetFailedLoginAttempt($user_id) {
+    $this->db->db_update('CORE_USER')->fields(array('FAILED_ATTEMPTS' => 0))->condition('USER_ID', $user_id)->execute();
   }
   
   public function changeRole($new_role_id) {
