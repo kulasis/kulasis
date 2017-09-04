56c3319e6b137a02ceb2fa5bb805032a26bbc643
Makoa Jacobsen <makoa@makoajacobsen.com>
2017-09-04 15:31:10 -0700

    Fix showing of transactions that don't matched passed type.

--- src/Core/Bundle/BillingBundle/Controller/CorePaymentsController.php
+++ src/Core/Bundle/BillingBundle/Controller/CorePaymentsController.php
@@ -176,13 +176,10 @@ class CorePaymentsController extends Controller {
         ->fields('applied', array('CONSTITUENT_APPLIED_PAYMENT_ID', 'CONSTITUENT_PAYMENT_ID', 'CONSTITUENT_TRANSACTION_ID', 'AMOUNT', 'ORIGINAL_AMOUNT'))
         ->condition('applied.CONSTITUENT_PAYMENT_ID', $payment_id)
         ->execute()->fetchAll();
-      
-      if ($payment['PAYMENT_TYPE'] == 'R')
-        $code_type = 'P';
-      else
-        $code_type = 'C';
+
+        $code_type = array('C', 'P');
     }
-    
+
     return $this->render('KulaCoreBillingBundle:CorePayments:payments_detail.html.twig', array('payment' => $payment, 'transactions' => $transactions, 'applied_payments' => $applied_payments, 'merchant_response' => $merchant_response, 'code_type' => $code_type));
   }
 
