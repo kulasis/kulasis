82e9c61ece8e43e2e9e0acf5268f25d2a67e0f20
Makoa Jacobsen <makoa@makoajacobsen.com>
2017-10-22 20:06:32 -0700

    SIS-386: Start adding transcript to teacher portal.

---
+++ src/HEd/Bundle/GradingBundle/Controller/TeacherTranscriptController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Kula\HEd\Bundle\GradingBundle\Controller;
+
+use Kula\Core\Bundle\FrameworkBundle\Controller\Controller;
+
+class TeacherTranscriptController extends Controller {
+  
+  public function indexAction() {
+    $this->authorize();
+    $this->setRecordType('Teacher.HEd.Advisor.Student');
+    
+    $transcript_data = array();
+    $transcript_schedule = array();
+    $transcript_degree = array();
+    
+    if ($this->record->getSelectedRecordID()) {
+      $transcript_service = $this->get('kula.HEd.grading.transcript');
+      $transcript_service->loadTranscriptForStudent($this->record->getSelectedRecordID());
+      $transcript_data = $transcript_service->getTranscriptData();
+      $transcript_schedule = $transcript_service->getCurrentScheduleData();
+      $transcript_degree = $transcript_service->getDegreeData();
+    }
+    return $this->render('KulaHEdGradingBundle:TeacherTranscript:transcript.html.twig', array('data' => $transcript_data, 'schedule' => $transcript_schedule, 'degrees' => $transcript_degree));
+  
+  }
+  
+}
